66af69e368edba9328ea95d52be2e99b11d5fef7
jfonte <jfontecha@gmail.com>
2019-08-24 06:22:39 -0600

    finished ch1

---
+++ readme.md
@@ -0,0 +1,2 @@
+"When action grows unprofitable, gather information; when infor- mation grows unprofitable, sleep."
+—Ursula K. Le Guin, The Left Hand of Darkness
---
+++ test.js
@@ -0,0 +1,28 @@
+//console.log("This is the first line\nAnd this is the second line");
+
+// escaping backslahes
+
+//console.log("A newline character is written like \"\\n\".");
+
+// ternary operator 
+
+//return (age < 18 ? true : false)
+    //function userIsYoungerThan18(age){
+//}
+
+// console.log(userIsYoungerThan18(4));
+
+// type conversion is INSANE
+
+//console.log(8*null);
+//console.log("5"-1);
+//console.log("5"+1);
+//console.log("five"*2);
+//console.log(false == 0);
+
+console.log(null || "user");
+console.log("agnes" || "user");
+
+console.log(null && "user");
+console.log("agnes" && "user");
+
